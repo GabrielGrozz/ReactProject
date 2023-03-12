b9aa2c6ad6fb53d93c411c09a505ec8b8e37bdd4
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-03-11 23:08:14 -0300

    add something

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import HelloWorld from "./Components/HelloWorld";
+import Explain from "./Components/Explain";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div className='game-container'>
-        <div className='about-container'>
-          <p className='about'>Sobre</p>
-        </div>
-      </div>
+    <div>
+      <HelloWorld />
+      <Explain />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
